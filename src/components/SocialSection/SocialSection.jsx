27615ca9a9d0c78943211601c74a2ca8aad8b53e
Ridel Saavedra Flores <ridel.saavedra@nfq.es>
2023-04-22 23:38:40 +0200

    añadidos estilos para el menu y el social bar

--- src/components/SocialSection/SocialSection.jsx
+++ src/components/SocialSection/SocialSection.jsx
@@ -3,13 +3,13 @@ import "./SocialSection.scss";
 
 export function SocialSection() {
   return (
-    <div class="flex-center">
-      <i class="fa fa-twitter fa-4x icon-3d"></i>
-      <i class="fa fa-youtube fa-4x icon-3d"></i>
-      <i class="fa fa-instagram fa-4x icon-3d"></i>
-      <i class="fa fa-twitch fa-4x icon-3d"></i>
-      <i class="fa-brands fa-tiktok fa-4x icon-3d"></i>
-      <i class="fa-brands fa-discord fa-4x icon-3d"></i>
+    <div className="flex-center">
+      <i className="fa fa-twitter fa-4x icon-3d"></i>
+      <i className="fa fa-youtube fa-4x icon-3d"></i>
+      <i className="fa fa-instagram fa-4x icon-3d"></i>
+      <i className="fa fa-twitch fa-4x icon-3d"></i>
+      <i className="fa-brands fa-tiktok fa-4x icon-3d"></i>
+      <i className="fa-brands fa-discord fa-4x icon-3d"></i>
     </div>
   );
 }
