35a127ae4ef4479e6d0ab047593d0334e0ba9994
Ridel Saavedra Flores <ridel.saavedra@nfq.es>
2023-06-10 00:53:11 +0200

    remove unused code

--- src/components/Dashboard/TopBarDashboard/TopBarDashboard.jsx
+++ src/components/Dashboard/TopBarDashboard/TopBarDashboard.jsx
@@ -2,18 +2,15 @@ import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../../theme/theme";
 
-import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 import LogoutIcon from "@mui/icons-material/Logout";
 import useAuth from "../../../hooks/useAuth";
 
 export function TopBarDashboard() {
   const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
 
   const { closeSession } = useAuth();
