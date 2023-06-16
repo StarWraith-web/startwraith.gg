53e8f6beb9a8c1426bb211680a6788c20e866fc9
Ridel Saavedra Flores <ridel.saavedra@nfq.es>
2023-06-16 18:26:12 +0200

    añadida política de privacidad

--- src/pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -7,7 +7,269 @@ export function PrivacyPolicy() {
       <div className="top-bar-container">
         <TopBar />
       </div>
-      <div className="main-content"></div>
+      <div className="main-content">
+        <div className="container-privacy">
+          <h1>Política de Privacidad</h1>
+          <p>
+            Star Wraith ("nosotros", "nuestro" o "nos") se compromete a proteger
+            su privacidad. Esta Política de privacidad explica cómo Star Wraith
+            recopila, usa y divulga su información personal.
+          </p>
+          <p>
+            Esta Política de privacidad se aplica a nuestro sitio web,
+            https://starwraith.gg y sus subdominios asociados (colectivamente,
+            nuestro "Servicio") junto con nuestra aplicación, Star Wraith. Al
+            acceder o utilizar nuestro Servicio, usted indica que ha leído,
+            comprendido y aceptado nuestra recopilación, almacenamiento, uso y
+            divulgación de su información personal como se describe en esta
+            Política de privacidad y en nuestros Términos de servicio.
+          </p>
+          <h2>Definiciones y Términos Clave</h2>
+          <p>
+            Para ayudar a explicar las cosas de la manera más clara posible en
+            esta Política de privacidad, cada vez que se hace referencia a
+            cualquiera de estos términos, se definen estrictamente como:
+          </p>
+          <ul>
+            <li>
+              <b>Cookie:</b> pequeña cantidad de datos generados por un sitio
+              web y guardados por su navegador web. Se utiliza para identificar
+              su navegador, proporcionar análisis, recordar información sobre
+              usted, como su preferencia de idioma o información de inicio de
+              sesión.
+            </li>
+            <li>
+              <b>Compañía:</b> cuando esta política menciona "Compañía",
+              "nosotros", "nos" o "nuestro", se refiere a Star Wraith, que es
+              responsable de su información en virtud de esta Política de
+              privacidad.
+            </li>
+            <li>
+              <b>Plataforma:</b> sitio web de Internet, aplicación web o
+              aplicación digital de cara al público de Star Wraith.
+            </li>
+            <li>
+              <b>País:</b> donde se encuentra Star Wraith o los propietarios /
+              fundadores de Star Wraith, en este caso es España.
+            </li>
+            <li>
+              <b>Cliente:</b> se refiere a la empresa, organización o persona
+              que se registra para utilizar el Servicio Star Wraith para
+              gestionar las relaciones con sus consumidores o usuarios del
+              servicio.
+            </li>
+            <li>
+              <b>Dispositivo:</b> cualquier dispositivo conectado a Internet,
+              como un teléfono, tablet, computadora o cualquier otro dispositivo
+              que se pueda usar para visitar Star Wraith y usar los servicios.
+            </li>
+            <li>
+              <b>Dirección IP:</b> a cada dispositivo conectado a Internet se le
+              asigna un número conocido como dirección de protocolo de Internet
+              (IP). Estos números generalmente se asignan en bloques
+              geográficos. A menudo, se puede utilizar una dirección IP para
+              identificar la ubicación desde la que un dispositivo se conecta a
+              Internet.
+            </li>
+            <li>
+              <b>Personal:</b> se refiere a aquellas personas que son empleadas
+              por Star Wraith o están bajo contrato para realizar un servicio en
+              nombre de una de las partes.
+            </li>
+            <li>
+              <b>Datos personales:</b> cualquier información que directa,
+              indirectamente o en conexión con otra información, incluido un
+              número de identificación personal, permita la identificación de
+              una persona física.
+            </li>
+            <li>
+              <b>Servicio:</b> se refiere al servicio brindado por Star Wraith
+              como se describe en los términos relativos (si están disponibles)
+              y en esta plataforma.
+            </li>
+            <li>
+              <b>Terceros:</b> se refiere a anunciantes, patrocinadores de
+              concursos, socios promocionales y de marketing, y otros que
+              brindan nuestro contenido o cuyos productos o servicios que
+              creemos que pueden interesarle.
+            </li>
+            <li>
+              <b>Sitio web:</b> el sitio de Star Wraith, al que se puede acceder
+              a través de esta URL: <b>https://starwraith.gg</b>
+            </li>
+            <li>
+              <b>Usted:</b> una persona o entidad que está registrada con Star
+              Wraith para utilizar los Servicios.
+            </li>
+          </ul>
+          <h2>Información recopilada automáticamente</h2>
+          <p>
+            Existe cierta información como su dirección de Protocolo de Internet
+            (IP) y / o las características del navegador y del dispositivo, que
+            se recopila automáticamente cuando visita nuestra plataforma. Esta
+            información se puede utilizar para conectar su computadora a
+            Internet. Otra información recopilada automáticamente podría ser un
+            inicio de sesión, dirección de correo electrónico, contraseña,
+            información de la computadora y la conexión, como los tipos y
+            versiones de complementos del navegador y la configuración de la
+            zona horaria, los sistemas operativos y las plataformas, el
+            historial de compras (a veces agregamos información similar de otros
+            usuarios), el flujo de clics completo del Localizador uniforme de
+            recursos (URL) hacia, a través y desde nuestro sitio web que puede
+            incluir la fecha y la hora; número de cookie; partes del sitio que
+            vio o buscó; y el número de teléfono que utilizó para llamar a
+            nuestro Servicio de atención al cliente. También podemos utilizar
+            datos del navegador como cookies, cookies Flash (también conocidas
+            como objetos compartidos locales Flash) o datos similares en ciertas
+            partes de nuestro sitio web para la prevención del fraude y otros
+            fines. Durante sus visitas, podemos utilizar herramientas de
+            software como JavaScript para medir y recopilar información de la
+            sesión, incluidos los tiempos de respuesta de la página, los errores
+            de descarga, la duración de las visitas a determinadas páginas, la
+            información de interacción de la página (como el desplazamiento, los
+            clics y el desplazamiento del mouse) y métodos utilizados para
+            navegar fuera de la página. También podemos recopilar información
+            técnica para ayudarnos a identificar su dispositivo con fines de
+            prevención y diagnóstico de fraude
+          </p>
+          <p>
+            Recopilamos automáticamente cierta información cuando visita, usa o
+            navega por la plataforma. Esta información no revela su identidad
+            específica (como su nombre o información de contacto), pero puede
+            incluir información de uso y dispositivo, como su dirección IP,
+            navegador y características del dispositivo, sistema operativo,
+            preferencias de idioma, URL de referencia, nombre del dispositivo,
+            país, ubicación. , información sobre quién y cuándo utiliza nuestra
+            información y otra información técnica. Esta información es
+            principalmente necesaria para mantener la seguridad y el
+            funcionamiento de nuestra plataforma, y para nuestros fines de
+            análisis e informes internos.
+          </p>
+          <h2>Venta de Negocio</h2>
+          <p>
+            Nos reservamos el derecho de transferir información a un tercero en
+            el caso de una venta, fusión u otra transferencia de todos o
+            sustancialmente todos los activos de Star Wraith o cualquiera de sus
+            afiliadas corporativas (como se define en este documento), o la
+            porción de Star Wraith o cualquiera de sus Afiliadas corporativas
+            con las que se relaciona el Servicio, o en el caso de que
+            discontinuemos nuestro negocio o presentemos una petición o hayamos
+            presentado una petición contra nosotros en caso de quiebra,
+            reorganización o procedimiento similar, siempre que el el tercero
+            acepte adherirse a los términos de esta Política de privacidad.
+          </p>
+          <h2>Afiliados</h2>
+          <p>
+            Podemos divulgar información (incluida la información personal)
+            sobre usted a nuestros afiliados corporativos. Para los propósitos
+            de esta Política de Privacidad, "Afiliado Corporativo" significa
+            cualquier persona o entidad que directa o indirectamente controla,
+            está controlada por o está bajo control común con Star Wraith, ya
+            sea por propiedad o de otra manera. Cualquier información
+            relacionada con usted que proporcionemos a nuestros afiliados
+            corporativos será tratada por dichos afiliados corporativos de
+            acuerdo con los términos de esta política de privacidad.
+          </p>
+          <h2>Ley que Rige</h2>
+          <p>
+            Esta Política de privacidad se rige por las leyes de España sin
+            tener en cuenta su disposición sobre conflicto de leyes. Usted
+            acepta la jurisdicción exclusiva de los tribunales en relación con
+            cualquier acción o disputa que surja entre las partes en virtud de
+            esta Política de privacidad o en relación con ella, excepto aquellas
+            personas que puedan tener derecho a presentar reclamaciones en
+            virtud del Escudo de privacidad o el marco suizo-estadounidense.
+          </p>
+          <p>
+            Las leyes de España, excluyendo sus conflictos de leyes, regirán
+            este Acuerdo y su uso de la plataforma. Su uso de la plataforma
+            también puede estar sujeto a otras leyes locales, estatales,
+            nacionales o internacionales.
+          </p>
+          <p>
+            Al usar Star Wraith o comunicarse con nosotros directamente,
+            significa que acepta esta Política de privacidad. Si no está de
+            acuerdo con esta Política de privacidad, no debe interactuar con
+            nuestro sitio web ni utilizar nuestros servicios. El uso continuo
+            del sitio web, la interacción directa con nosotros o después de la
+            publicación de cambios en esta Política de privacidad que no afecten
+            significativamente el uso o divulgación de su información personal
+            significará que acepta esos cambios.
+          </p>
+          <h2>Tu consentimiento</h2>
+          <p>
+            Hemos actualizado nuestra Política de privacidad para brindarle
+            total transparencia sobre lo que se establece cuando visita nuestro
+            sitio y cómo se utiliza. Al utilizar nuestra plataforma, registrar
+            una cuenta o realizar una compra, por la presente acepta nuestra
+            Política de privacidad y acepta sus términos.
+          </p>
+          <h2>Enlaces a otros Sitios Web</h2>
+          <p>
+            Esta Política de privacidad se aplica solo a los Servicios. Los
+            Servicios pueden contener enlaces a otros sitios web que Star Wraith
+            no opera ni controla. No somos responsables por el contenido, la
+            precisión o las opiniones expresadas en dichos sitios web, y dichos
+            sitios web no son investigados, monitoreados o verificados por
+            nuestra precisión o integridad. Recuerde que cuando utiliza un
+            enlace para ir desde los Servicios a otro sitio web, nuestra
+            Política de privacidad deja de estar en vigor. Su navegación e
+            interacción en cualquier otro sitio web, incluidos aquellos que
+            tienen un enlace en nuestra plataforma, están sujetos a las propias
+            reglas y políticas de ese sitio web. Dichos terceros pueden utilizar
+            sus propias cookies u otros métodos para recopilar información sobre
+            usted.
+          </p>
+          <h2>Cambios en nuestra Política de Privacidad</h2>
+          <p>
+            Podemos cambiar nuestro Servicio y nuestras políticas, y es posible
+            que debamos realizar cambios en esta Política de privacidad para que
+            reflejen con precisión nuestro Servicio y nuestras políticas. A
+            menos que la ley exija lo contrario, le notificaremos (por ejemplo,
+            a través de nuestro Servicio) antes de realizar cambios en esta
+            Política de privacidad y le daremos la oportunidad de revisarlos
+            antes de que entren en vigencia. Luego, si continúa utilizando el
+            Servicio, estará sujeto a la Política de privacidad actualizada. Si
+            no desea aceptar esta o cualquier Política de privacidad
+            actualizada, puede eliminar su cuenta.
+          </p>
+          <h2>Servicios de terceros</h2>
+          <p>
+            Podemos mostrar, incluir o poner a disposición contenido de terceros
+            (incluidos datos, información, aplicaciones y otros servicios de
+            productos) o proporcionar enlaces a sitios web o servicios de
+            terceros ("Servicios de terceros").
+          </p>
+          <p>
+            Usted reconoce y acepta que Star Wraith no será responsable de
+            ningún Servicio de terceros, incluida su precisión, integridad,
+            puntualidad, validez, cumplimiento de los derechos de autor,
+            legalidad, decencia, calidad o cualquier otro aspecto de los mismos.
+            Star Wraith no asume ni tendrá ninguna obligación o responsabilidad
+            ante usted o cualquier otra persona o entidad por los Servicios de
+            terceros.
+          </p>
+          <p>
+            Los Servicios de terceros y los enlaces a los mismos se brindan
+            únicamente para su conveniencia y usted accede a ellos y los usa
+            completamente bajo su propio riesgo y sujeto a los términos y
+            condiciones de dichos terceros.
+          </p>
+          <h2>Contáctenos</h2>
+          <p>
+            No dude en contactarnos si tiene alguna pregunta sobre nuestra
+            Política de Cookies.
+          </p>
+          <ul>
+            <li>
+              A través de correo electrónico: <b>rjbm90@gmail.com</b>
+            </li>
+            <li>
+              A través de este enlace: <b>https://starwraith.gg/home</b>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className="bottom-section">
         <div className="footer-section">
           <Footer />
